ff852bb4fa3d674368a0ebb3452ea11aa3bca006
benjamin <b.james.neustadt@gmail.com>
2023-03-01 23:42:35 +0100

    WIP: passing for 4 inner values

--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -1,17 +1,15 @@
 require 'csv'
-
 def data_parse(csv_file)
-
   csv_table = CSV.parse(csv_file, headers: true)
   data = {}
-
   csv_table.by_row.each do |row|
-
     unless row["Entry.status"] == "Removed"
 
+      #BIG HASH
       device_type = row["Device_Type.name"]
       manufacturer = row["Device_Model.manufacturer_identifier"]
       model_hardware_version = row["Device_Model.model_identifier_concatenated_with_hardware_version"]
+
       # INNER HASH
       firmware_version = row["Device_Model.firmware_version"]
       smets_chts_version = row["SMETS_CHTS Version.Version_number_and_effective_date"]
@@ -19,12 +17,12 @@ def data_parse(csv_file)
       image_hash = row["Manufacturer_Image.hash"]
       data[device_type] ||= {}
       data[device_type][manufacturer] ||= {}
-      #unless data[device_type][manufacturer][model_hardware_version]
 
       data[device_type][manufacturer][model_hardware_version] ||= {}
       if data[device_type][manufacturer][model_hardware_version].length > 0
+        
         # Must transform the previous firmware hash into the new hash format
-        previous_hash = {
+        data[device_type][manufacturer][model_hardware_version][firmware_version] ||= {
           data[device_type][manufacturer][model_hardware_version][:gbcs_version] => {
             smets_chts_version: data[device_type][manufacturer][model_hardware_version][:smets_chts_version],
             image_hash: data[device_type][manufacturer][model_hardware_version][:image_hash]
@@ -37,19 +35,24 @@ def data_parse(csv_file)
             image_hash: image_hash
           }
         }
-        # This will merge the previous and the current hash so we end up with an array of hashes
-        data[device_type][manufacturer][model_hardware_version]["firmware_version"] ||= previous_hash.merge(inner_hash)
+        # This will merge the previous and the current hash into the firmware_version hash so we end up with an array of hashes
+        data[device_type][manufacturer][model_hardware_version][firmware_version].merge!(inner_hash)
         # Then we need to delete the previous keys that were saved on the first row since they are now nested under the key 'firmware_version'
-        data[device_type][manufacturer][model_hardware_version].delete_if{|k,v| k != "firmware_version"}
         puts data
+        data[device_type][manufacturer][model_hardware_version].delete_if{|k,v| k.is_a?(Symbol)}
+        puts data
+
       else
+
         inner_hash = {
             firmware_version: firmware_version,                          
             smets_chts_version: smets_chts_version, 
             gbcs_version: gbcs_version,              
             image_hash: image_hash
           }
+
         data[device_type][manufacturer][model_hardware_version] = inner_hash
+
       end
     end
   end
