ecdafc6be495ab80724a5ae4f8342c2bf391f786
benjamin <b.james.neustadt@gmail.com>
2023-03-01 17:53:21 +0100

    Modify data_parse
    * Should not allow incoming hash to be overwritten,
    we therefore need to ask if the hash key is already in use,
    or whether we should create a new one:
    
    With this syntax:
    
        data[device_type] ||= {}
    
    We say:
    
      "it either exists or make a new empty hash with this as the key
      to any value"

--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -1,25 +1,26 @@
 require 'csv'
-def data_parse(string_input)
+def data_parse(csv_file)
 
-  csv_table = CSV.parse(string_input, headers: true)
+  csv_table = CSV.parse(csv_file, headers: true)
 
   data = {}
   csv_table.by_row.each do |row|
+    unless row["Entry.status"] == "Removed"
+    device_type = row["Device_Type.name"]
+    manufacturer = row["Device_Model.manufacturer_identifier"]
+    model_hardware_version = row["Device_Model.model_identifier_concatenated_with_hardware_version"]
 
-    specific_row = {
-      row["Device_Type.name"] => {
-        row["Device_Model.manufacturer_identifier"] => {
-          row["Device_Model.model_identifier_concatenated_with_hardware_version"] => {
+    # If the key is not already in use, we begin building the outermost key
+    data[device_type] ||= {}
+    data[device_type][manufacturer] = {}
+    data[device_type][manufacturer][model_hardware_version] = {
             firmware_version: row["Device_Model.firmware_version"],                          # J (Device_Model.firmware_version)
             smets_chts_version: row["SMETS_CHTS Version.Version_number_and_effective_date"], # K (SMETS_CHTS Version.Version_number_and_effective_date)
             gbcs_version: row["GBCS Version.version_number"],                                # L (GBCS Version.version_number)
             image_hash: row["Manufacturer_Image.hash"]                                       # M (Manufacturer_Image.hash)
-          }
-        }
-      }
     }
-    # We can put the removal here. although I would rather put the check at the beginning - though this is easiest to do for now
-    data.merge!(specific_row) unless row["Entry.status"] == "Removed"
+    end
   end
   data
 end
+
