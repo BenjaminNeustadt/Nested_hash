0018cc49e90c556ac1e57ee061c8cff1f424ec3a
benjamin <b.james.neustadt@gmail.com>
2023-03-01 17:38:12 +0100

    data_parse parses three rows(different keys)

--- spec/csv_to_hash_spec.rb
+++ spec/csv_to_hash_spec.rb
@@ -85,7 +85,7 @@ RSpec.describe '#data_parse' do
     expect(actual).to eq expected
   end
 
-  it 'parses multiple rows that have different keys' do
+  it 'parses two rows that have different keys' do
 
     input_csv =
     <<~CSV
@@ -115,6 +115,53 @@ RSpec.describe '#data_parse' do
           }
         }
       },
+    }
+
+    actual = data_parse(input_csv)
+    expect(actual).to eq expected
+  end
+
+  it 'parses three rows that have different keys' do
+
+    input_csv =
+    <<~CSV
+      Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
+      Version 1,1,Current,Type_Communications,Mfg_1,Model_1,1.0.0,AC,1.1.0,1.1.1,CHTS V1,GBCS Version 1,image1hash
+      Version 2,2,Current,Type_Meter,Mfg_2,Model_2,2.0.0,AC,2.2.0,2.2.2,CHTS V2,GBCS Version 2,image2hash
+      Version 3,3,Current,Type_Boiler,Mfg_3,Model_3,3.0.0,AC,3.3.0,3.3.3,CHTS V3,GBCS Version 3,image3hash
+    CSV
+
+    expected = {
+      'Type_Communications' => {            # D (Device_Type.name)
+        'Mfg_1' => {                        # E (Device_Model.manufacturer_identifier)
+          '1.1.0' => {                      # I (Device_Model.model_identifier_concatenated_with_hardware_version)
+            firmware_version: '1.1.1',      # J (Device_Model.firmware_version)
+            smets_chts_version: 'CHTS V1',  # K (SMETS_CHTS Version.Version_number_and_effective_date)
+            gbcs_version: 'GBCS Version 1', # L (GBCS Version.version_number)
+            image_hash: 'image1hash'        # M (Manufacturer_Image.hash)
+          }
+        }
+      },
+      'Type_Meter' => {                     # D (Device_Type.name)
+        'Mfg_2' => {                        # E (Device_Model.manufacturer_identifier)
+          '2.2.0' => {                      # I (Device_Model.model_identifier_concatenated_with_hardware_version)
+            firmware_version: '2.2.2',      # J (Device_Model.firmware_version)
+            smets_chts_version: 'CHTS V2',  # K (SMETS_CHTS Version.Version_number_and_effective_date)
+            gbcs_version: 'GBCS Version 2', # L (GBCS Version.version_number)
+            image_hash: 'image2hash'        # M (Manufacturer_Image.hash)
+          }
+        }
+      },
+      'Type_Boiler' => {
+        'Mfg_3' => {
+          '3.3.0' => {
+            firmware_version: '3.3.3',
+            smets_chts_version: 'CHTS V3',
+            gbcs_version: 'GBCS Version 3',
+            image_hash: 'image3hash'
+          }
+        }
+      }
     }
 
     actual = data_parse(input_csv)
