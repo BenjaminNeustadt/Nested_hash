bedb5a6e9d9ce8223a605cd936c0e2a525a11321
benjamin <b.james.neustadt@gmail.com>
2023-03-01 17:36:11 +0100

    data_parse method iterates over multiple rows (different keys)

--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -2,20 +2,24 @@ require 'csv'
 def data_parse(string_input)
 
   csv_table = CSV.parse(string_input, headers: true)
-  row_1 = csv_table[0]
 
+  data = {}
+  csv_table.by_row.each do |row|
 
-  {
-    row_1["Device_Type.name"] => {
-    row_1["Device_Model.manufacturer_identifier"] => {
-    row_1["Device_Model.model_identifier_concatenated_with_hardware_version"] => {
-           firmware_version: row_1["Device_Model.firmware_version"], # J (Device_Model.firmware_version)
-           smets_chts_version: row_1["SMETS_CHTS Version.Version_number_and_effective_date"],     # K (SMETS_CHTS Version.Version_number_and_effective_date)
-           gbcs_version: row_1["GBCS Version.version_number"],    # L (GBCS Version.version_number)
-           image_hash: row_1["Manufacturer_Image.hash"]         # M (Manufacturer_Image.hash)
-         }
-       }
-     }
-   }
+    specific_row = {
+      row["Device_Type.name"] => {
+        row["Device_Model.manufacturer_identifier"] => {
+          row["Device_Model.model_identifier_concatenated_with_hardware_version"] => {
+            firmware_version: row["Device_Model.firmware_version"],                          # J (Device_Model.firmware_version)
+            smets_chts_version: row["SMETS_CHTS Version.Version_number_and_effective_date"], # K (SMETS_CHTS Version.Version_number_and_effective_date)
+            gbcs_version: row["GBCS Version.version_number"],                                # L (GBCS Version.version_number)
+            image_hash: row["Manufacturer_Image.hash"]                                       # M (Manufacturer_Image.hash)
+          }
+        }
+      }
+    }
 
+    data.merge!(specific_row)
+  end
+  data
 end
