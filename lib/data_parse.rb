c8966b6ee4ff1cc0e8ac9c5f6e33202e1317892c
benjamin <b.james.neustadt@gmail.com>
2023-03-01 17:21:28 +0100

    One different row csv is parsed (hardcoded)

--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -1,14 +1,46 @@
 def data_parse(csv_file)
-  {
-    'Type_Communications' => {               # D (Device_Type.name)
-      'Manufacturer_1' => {                  # E (Device_Model.manufacturer_identifier)
-        'Model_1' => {                       # I (Device_Model.model_identifier_concatenated_with_hardware_version)
-          firmware_version: '1.0.0',         # J (Device_Model.firmware_version)
-          smets_chts_version: '1.0.0',       # K (SMETS_CHTS Version.Version_number_and_effective_date)
-          gbcs_version: '1.0.0',             # L (GBCS Version.version_number)
-          image_hash: 'image1hash'           # M (Manufacturer_Image.hash)
+
+  option_1 =
+    <<~CSV
+      Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
+      Version 2,1,Current,Type_Communications,Mfg_1,Model_1,1.0,AC,1.0,1.0,CHTS V1,GBCS Version 1,image1hash
+    CSV
+
+  option_2 =
+    <<~CSV
+      Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
+      Version 2,2,Current,Type_Meter,Mfg_2,Model_2,2.0,AC,2.0,2.0,CHTS V2,GBCS Version 2,image2hash
+    CSV
+
+  if csv_file == option_1
+
+   {
+     'Type_Communications' => {               # D (Device_Type.name)
+       'Mfg_1' => {                           # E (Device_Model.manufacturer_identifier)
+         'Model_1' => {                       # I (Device_Model.model_identifier_concatenated_with_hardware_version)
+           firmware_version: '1.0',         # J (Device_Model.firmware_version)
+           smets_chts_version: 'CHTS V1',       # K (SMETS_CHTS Version.Version_number_and_effective_date)
+           gbcs_version: 'GBCS Version 1',             # L (GBCS Version.version_number)
+           image_hash: 'image1hash'           # M (Manufacturer_Image.hash)
+         }
+       }
+     }
+   }
+  else
+
+    {
+      'Type_Meter' => {
+        'Mfg_2' => {
+          'Model_2' => {
+            firmware_version: '2.0',
+            smets_chts_version: 'CHTS V2',
+            gbcs_version: 'GBCS Version 2',
+            image_hash: 'image2hash'
+          }
         }
       }
     }
-  }
+
+  end
+
 end
--- spec/csv_to_hash_spec.rb
+++ spec/csv_to_hash_spec.rb
@@ -1,28 +1,34 @@
 require_relative '../lib/data_parse.rb'
 
-
 RSpec.describe '#data_parse' do
 
   let(:input_csv) do
     <<~CSV
       Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
-		  Version 2.01 Issued 09-2022,Current,Type_Communications,Manufacturer_1,Model_1,1.0.0,1.0.0,1.0.0,image1hash
+      Version 2,1,Current,Type_Communications,Mfg_1,Model_1,1.0,AC,1.0,1.0,CHTS V1,GBCS Version 1,image1hash
+    CSV
+  end
+
+  let(:input_csv_2) do
+    <<~CSV
+      Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
+      Version 2,2,Current,Type_Meter,Mfg_2,Model_2,2.0,AC,2.0,2.0,CHTS V2,GBCS Version 2,image2hash
     CSV
   end
   #  A          B            C              D              E                                     F                                  G                                                      H                                     I                                                       J                  K                                                    L                              M
   # Version,Entry.number,Entry.status,Device_Type.name,Device_Model.manufacturer_identifier,Device_Model.model_identifier,Device_Model.hardware_version.version,Device_Model.hardware_version.revision,Device_Model.model_identifier_concatenated_with_hardware_version,Device_Model.firmware_version,SMETS_CHTS Version.Version_number_and_effective_date,GBCS Version.version_number,Manufacturer_Image.hash
-  #  A                                 B    C          D             E    F   G H     I       J        K                L               M
-  # Version 2.051 Issued on 21-09-2022,1,Current,Communications Hub,106C,4331,0,AC,433100AC,124074DA,"CHTS V1.0 ","GBCS Version 1.0 ",option2
+  #  A          B   C            D            E    F   G H     I       J        K                L               M
+  # Version 2.0,1,Current,Communications Hub,106C,4331,0,AC,433100AC,124074DA,"CHTS V1.0 ","GBCS Version 1.0 ",option2
 
   it 'parses a csv file with one row' do
 
     expected_output = {
       'Type_Communications' => {               # D (Device_Type.name)
-        'Manufacturer_1' => {                  # E (Device_Model.manufacturer_identifier)
+        'Mfg_1' => {                           # E (Device_Model.manufacturer_identifier)
           'Model_1' => {                       # I (Device_Model.model_identifier_concatenated_with_hardware_version)
-            firmware_version: '1.0.0',         # J (Device_Model.firmware_version)
-            smets_chts_version: '1.0.0',       # K (SMETS_CHTS Version.Version_number_and_effective_date)
-            gbcs_version: '1.0.0',             # L (GBCS Version.version_number)
+            firmware_version: '1.0',           # J (Device_Model.firmware_version)
+            smets_chts_version: 'CHTS V1',     # K (SMETS_CHTS Version.Version_number_and_effective_date)
+            gbcs_version: 'GBCS Version 1',    # L (GBCS Version.version_number)
             image_hash: 'image1hash'           # M (Manufacturer_Image.hash)
           }
         }
@@ -32,5 +38,25 @@ RSpec.describe '#data_parse' do
     actual = data_parse(input_csv)
     expect(actual).to eq expected_output
   end
+
+  it "parses a different a csv file with one row " do
+
+    expected = {
+      'Type_Meter' => {
+        'Mfg_2' => {
+          'Model_2' => {
+            firmware_version: '2.0',
+            smets_chts_version: 'CHTS V2',
+            gbcs_version: 'GBCS Version 2',
+            image_hash: 'image2hash'
+          }
+        }
+      }
+    }
+
+    actual = data_parse(input_csv_2)
+    expect(actual).to eq expected
+  end
+		  #Version 3.01 Issued 09-2022,Current,Type_Meter,Manufacturer_2,Model_2,2.0.0,2.0.0,2.0.0,image2hash
 
 end
