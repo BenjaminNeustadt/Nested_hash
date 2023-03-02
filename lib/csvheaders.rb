9230d81bd3a9610b5efa8fd7920aa9700177d466
benjamin <b.james.neustadt@gmail.com>
2023-03-02 17:21:19 +0100

    Extract class and module to separate files

---
+++ lib/csvheaders.rb
@@ -0,0 +1,20 @@
+module CSVHeaders 
+
+  OUTER_HASH_VALUES =  {
+
+    device_type:              "Device_Type.name",
+    manufacturer:             "Device_Model.manufacturer_identifier",
+    model_hardware_version:   "Device_Model.model_identifier_concatenated_with_hardware_version",
+
+  }
+
+  INNER_HASH_VALUES = {
+
+    firmware_version:         "Device_Model.firmware_version",
+    smets_chts_version:       "SMETS_CHTS Version.Version_number_and_effective_date",
+    gbcs_version:             "GBCS Version.version_number",
+    image_hash:               "Manufacturer_Image.hash"
+
+  }
+
+end
--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -1,115 +1,6 @@
 require 'csv'
+require_relative './row.rb'
 
-$INFO = true
-
-module CSVHeaders 
-
-  OUTER_HASH_VALUES =  {
-
-    device_type:              "Device_Type.name",
-    manufacturer:             "Device_Model.manufacturer_identifier",
-    model_hardware_version:   "Device_Model.model_identifier_concatenated_with_hardware_version",
-
-  }
-
-  INNER_HASH_VALUES = {
-
-    firmware_version:         "Device_Model.firmware_version",
-    smets_chts_version:       "SMETS_CHTS Version.Version_number_and_effective_date",
-    gbcs_version:             "GBCS Version.version_number",
-    image_hash:               "Manufacturer_Image.hash"
-
-  }
-
-end
-
-class Row
-  include CSVHeaders
-
-  def initialize(row)
-    @row = row
-  end
-
-  attr_reader :row
-
-  def device_type
-    row[OUTER_HASH_VALUES[:device_type]]
-  end
-
-  def manufacturer
-    row[OUTER_HASH_VALUES[:manufacturer]]
-  end
-
-  def model_hardware_version
-    row[OUTER_HASH_VALUES[:model_hardware_version]]
-  end
-
-  # INNER HASH
-  def firmware_version
-    row[INNER_HASH_VALUES[:firmware_version]]
-  end
-
-  def smets_chts_version
-    row[INNER_HASH_VALUES[:smets_chts_version]]
-  end
-
-  def gbcs_version
-    row[INNER_HASH_VALUES[:gbcs_version]]
-  end
-
-  def image_hash
-    row[INNER_HASH_VALUES[:image_hash]]
-  end
-
-## Creating hashes if they dont exist
-  def device_type_key(data)
-    data[self.device_type] ||= {}
-  end
-
-  def manufacturer_key(data)
-    data[self.device_type][self.manufacturer] ||= {}
-  end
-
-  def model_hardware_version_key(data)
-    data[self.device_type][self.manufacturer][self.model_hardware_version] ||= {}
-  end
-
-  def inner_hash
-
-    inner_hash = {
-      firmware_version: self.firmware_version,                          
-      smets_chts_version: self.smets_chts_version, 
-      gbcs_version: self.gbcs_version,              
-      image_hash: self.image_hash
-    }
-
-  end
-
-  def build_array_for_inner_hashes(data) 
-
-##  We check if the value is already an array and append to it if so.
-    if data[self.device_type][self.manufacturer][self.model_hardware_version].is_a?(Array)
-      data[self.device_type][self.manufacturer][self.model_hardware_version] << inner_hash
-    else
-##  Otherwise we create a new hash
-      data[self.device_type][self.manufacturer][self.model_hardware_version] = [data[self.device_type][self.manufacturer][self.model_hardware_version], inner_hash]
-    end
-
-  end
-
-  def innermost_standard(data)
-
-    data[self.device_type][self.manufacturer][self.model_hardware_version] = inner_hash
-
-  end
-
-  def create_hash_structures(data)
-      self.device_type_key(data)
-      self.manufacturer_key(data)
-      self.model_hardware_version_key(data)
-  end
-
-end
 
 # The functionality of what a Row has has been abstracted to Row class,
 def data_parse(csv_file)
@@ -120,7 +11,6 @@ def data_parse(csv_file)
     unless row["Entry.status"] == "Removed"
 
       row_object = Row.new(row)
-
 
 ## Creates the hash structure if it does not already exist
       row_object.create_hash_structures(data)
