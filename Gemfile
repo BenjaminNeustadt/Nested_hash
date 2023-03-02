257e028765eedec85ede3f9f7adcfac9451c5a85
benjamin <b.james.neustadt@gmail.com>
2023-03-02 18:52:30 +0100

    Configure executable
    * Add executable in bin/
    * Allow executable to take argument

--- Gemfile
+++ Gemfile
@@ -7,3 +7,5 @@ source "https://rubygems.org"
 gem "rspec", "~> 3.12"
 
 gem "simplecov", "~> 0.22.0"
+
+gem "colorize", "~> 0.8.1"
---
+++ bin/csv-to-hash.rb
@@ -0,0 +1,30 @@
+#!/usr/bin/env ruby
+
+require_relative '../lib/data_parse.rb'
+require 'colorize'
+
+
+def process
+
+  puts "Data is  successcullfy transformed into Hash".cyan
+
+end
+
+def hashify_csv(csv_file)
+  file = File.new(csv_file, 'r')
+
+  File.open('can_write_data.rb', 'w') do |f|
+    result = data_parse(file)
+    f.puts "#{result}"
+  end
+
+  process
+
+rescue Errno::ENOENT => e
+  puts "An error occured - check your file exists and its location.".red
+  exit(1)
+end
+
+document = 'Ctral-Products-Lis.csv'
+
+hashify_csv(document)
