840895a7172011bfc9329562125b966909c148e6
benjamin <b.james.neustadt@gmail.com>
2023-03-12 19:03:45 +0100

    Remove explicit reference to self
    * Refactor predicate methods

--- lib/row.rb
+++ lib/row.rb
@@ -10,50 +10,67 @@ class Row
 
 ## Creating hashes if they dont exist
   def device_type_key(data)
-    data[self.device_type] ||= {}
+    data[device_type] ||= {}
   end
 
   def manufacturer_key(data)
-    data[self.device_type][self.manufacturer] ||= {}
+    data[device_type][manufacturer] ||= {}
   end
 
   def model_hardware_version_key(data)
-    data[self.device_type][self.manufacturer][self.model_hardware_version] ||= {}
+    data[device_type][manufacturer][model_hardware_version] ||= {}
   end
 
   def inner_hash
 
     inner_hash = {
-      firmware_version: self.firmware_version,
-      smets_chts_version: self.smets_chts_version,
-      gbcs_version: self.gbcs_version,
-      image_hash: self.image_hash
+      firmware_version: firmware_version,
+      smets_chts_version: smets_chts_version,
+      gbcs_version: gbcs_version,
+      image_hash: image_hash
     }
 
   end
 
+  def _model_hardware_version(data)
+    data[device_type][manufacturer][model_hardware_version]
+  end
+
+  def item_is_collection?(data)
+    _model_hardware_version(data).is_a?(Array)
+  end
+
+  # def item
+  #   data[self.device_type][self.manufacturer][self.model_hardware_version]
+  # end
+
+  def item_is_collection?(data)
+    data[device_type][manufacturer][model_hardware_version].is_a?(Array)
+  end
+
   def build_array_for_inner_hashes(data)
 
+    item = data[device_type][manufacturer][model_hardware_version]
 ##  We check if the value is already an array and append to it if so.
-    if data[self.device_type][self.manufacturer][self.model_hardware_version].is_a?(Array)
-      data[self.device_type][self.manufacturer][self.model_hardware_version] << inner_hash
+    if item_is_collection?(data) 
+      item << inner_hash
     else
 ##  Otherwise we create a new hash
-      data[self.device_type][self.manufacturer][self.model_hardware_version] = [data[self.device_type][self.manufacturer][self.model_hardware_version], inner_hash]
+      data[device_type][manufacturer][model_hardware_version] = [item, inner_hash]
     end
 
   end
 
   def innermost_standard(data)
 
-    data[self.device_type][self.manufacturer][self.model_hardware_version] = inner_hash
+    data[device_type][manufacturer][model_hardware_version] = inner_hash
 
   end
 
   def create_hash_structures(data)
-      self.device_type_key(data)
-      self.manufacturer_key(data)
-      self.model_hardware_version_key(data)
+      device_type_key(data)
+      manufacturer_key(data)
+      model_hardware_version_key(data)
   end
 
 end
