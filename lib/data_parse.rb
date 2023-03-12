bd13097cfbef8c444dab846f1117963025e0a1bf
benjamin <b.james.neustadt@gmail.com>
2023-03-12 20:02:41 +0100

    Rename method for meaning
    * Refactor the block variable name to 'r', although it doesn't seem like
      an idiomatic name attribute, it allows us to use "row" name for an
      actual instance of the Row class.

--- lib/data_parse.rb
+++ lib/data_parse.rb
@@ -1,29 +1,34 @@
 require 'csv'
 require_relative './row'
 
+def model_hardware_exists?(object, data)
+  object.model_hardware_version_key(data).length.positive?
+end
+
 ## The functionality of what a Row has has been abstracted to Row class
 def data_parse(csv_file)
   csv_table = CSV.parse(csv_file, headers: true)
   data = {}
 
-  csv_table.by_row.each do |row|
-    unless row['Entry.status'] == 'Removed'
+  csv_table.by_row.each do |r|
+    unless r['Entry.status'] == 'Removed'
 
-      row_object = Row.new(row)
+      row = Row.new(r)
 
       ## Creates the hash structure if it does not already exist
-      row_object.create_hash_structures(data)
+      row.create_hash_structures(data)
 
-      if row_object.model_hardware_version_key(data).length.positive?
+
+      if model_hardware_exists?(row, data)
 
         ## Build an array with the key of the existant inner hash as a key for
         #  the array containing as many inner_hashes as necessary
-        row_object.build_array_for_inner_hashes(data)
+        row.build_array_for_inner_hashes(data)
 
       else
         ## Otherwise just give the standard inner hash as a single value
         #  belonging to key
-        row_object.innermost_standard(data)
+        row.innermost_standard(data)
 
       end
     end
