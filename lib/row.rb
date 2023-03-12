1e074c78e300cdbe105d931431211f1cca8848f3
benjamin <b.james.neustadt@gmail.com>
2023-03-12 20:14:19 +0100

    Visibility
    *Interface limitation, Encapsulation: we hide things away that don't
    need to be usable or visible in all the program

--- lib/row.rb
+++ lib/row.rb
@@ -2,11 +2,13 @@ require_relative './csvheaders.rb'
 class Row
   include CSVHeaders
 
+  private
+
+  attr_reader :row
+
   def initialize(row)
     @row = row
   end
-
-  attr_reader :row
 
 ## Creating hashes if they dont exist
   def device_type_key(data)
@@ -17,60 +19,52 @@ class Row
     data[device_type][manufacturer] ||= {}
   end
 
-  def model_hardware_version_key(data)
-    data[device_type][manufacturer][model_hardware_version] ||= {}
-  end
-
   def inner_hash
-
-    inner_hash = {
+    {
       firmware_version: firmware_version,
       smets_chts_version: smets_chts_version,
       gbcs_version: gbcs_version,
       image_hash: image_hash
     }
-
   end
 
+  # predicate helper methods
   def _model_hardware_version(data)
     data[device_type][manufacturer][model_hardware_version]
   end
 
-  def item_is_collection?(data)
+  def model_hardware_version_value_is_collection_on?(data)
     _model_hardware_version(data).is_a?(Array)
   end
 
-  # def item
-  #   data[self.device_type][self.manufacturer][self.model_hardware_version]
-  # end
+  def assign_model_hardware_version_on(data)
+    data[device_type][manufacturer][model_hardware_version] = [_model_hardware_version(data), inner_hash]
+  end
 
-  def item_is_collection?(data)
-    data[device_type][manufacturer][model_hardware_version].is_a?(Array)
+  public
+
+  def model_hardware_version_key(data)
+    data[device_type][manufacturer][model_hardware_version] ||= {}
   end
 
   def build_array_for_inner_hashes(data)
-
-    item = data[device_type][manufacturer][model_hardware_version]
-##  We check if the value is already an array and append to it if so.
-    if item_is_collection?(data) 
-      item << inner_hash
+    ##  We check if the value is already an array and append to it if so.
+    if model_hardware_version_value_is_collection_on?(data)
+      _model_hardware_version(data) << inner_hash
     else
-##  Otherwise we create a new hash
-      data[device_type][manufacturer][model_hardware_version] = [item, inner_hash]
+      ##  Otherwise we create a new hash
+      assign_model_hardware_version_on(data)
     end
-
   end
 
   def innermost_standard(data)
-
     data[device_type][manufacturer][model_hardware_version] = inner_hash
-
   end
 
   def create_hash_structures(data)
-      device_type_key(data)
-      manufacturer_key(data)
-      model_hardware_version_key(data)
+    device_type_key(data)
+    manufacturer_key(data)
+    model_hardware_version_key(data)
   end
 
 end
