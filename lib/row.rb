53f28542ed563a02f1519931de60ec184847255b
benjamin <b.james.neustadt@gmail.com>
2023-03-13 14:34:59 +0100

    WIP: require_relative is necessary

--- lib/row.rb
+++ lib/row.rb
@@ -1,6 +1,6 @@
-require 'csvheaders'
-require 'model_hardware_version_helpers'
-require 'hash_creation_helpers'
+require_relative './csvheaders.rb'
+require_relative './model_hardware_version_helpers'
+require_relative './hash_creation_helpers'
 
 class Row
   include CSVHeaders
